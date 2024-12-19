695d22700713c83073df5ac764e4a234534e1b15
Siju Sakarias <sijus887@gmail.com>
2024-12-18 18:53:29 -0600

    Sort items by digits

--- app/src/main/java/com/siju/networksample/domain/usecase/GetItemsUseCase.kt
+++ app/src/main/java/com/siju/networksample/domain/usecase/GetItemsUseCase.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.siju.networksample.data.repository.ItemRepository
 import com.siju.networksample.domain.model.Item
 
+private const val DELIMITER = "Item "
 class GetItemsUseCase(
     private val repository: ItemRepository
 ) {
@@ -13,6 +14,10 @@ class GetItemsUseCase(
         // Filter out items with blank or null names
         val filtered = items.filter { !it.name.isNullOrBlank() }
         // Sort by listId and then by name
-        return filtered.sortedWith(compareBy<Item> { it.listId }.thenBy { it.name })
+        return filtered.sortedWith(compareBy<Item> { it.listId }.thenBy {
+            // Extract the digits from the name, assuming the format is "Item <number>"
+            val numericPart = it.name?.substringAfter(DELIMITER)?.toIntOrNull() ?: Int.MAX_VALUE
+            numericPart
+        })
     }
 }
